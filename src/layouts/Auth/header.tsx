4e99afc1a2c47f1965eebb1695a0a47d99cb4834
Kauan Pontes <kauanps2271@gmail.com>
2026-05-02 18:17:17 -0300

    atualizacao

--- src/layouts/Auth/header.tsx
+++ src/layouts/Auth/header.tsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+import helpFlowLogo from "../../assets/images/logos/android-chrome-512x512.png";
+import { Menu, X } from "lucide-react";
+
+import { Link } from "react-router-dom";
+
 export default function Header() {
-    return (
-        <h1>Header</h1>
-    )
+  return (
+    <header className="bg-white shadow-sm mb-1">
+      <div className="max-w-6xl mx-auto flex items-center justify-between px-4 py-4">
+        {/* Logo + Nome */}
+        <div className="flex items-center gap-3">
+          <img src={helpFlowLogo} className="w-8 h-8" />
+
+          <Link to="/" className="text-xl font-bold">
+            <span style={{ color: "#216bde" }}>Help</span>
+            <span style={{ color: "#15a059" }}>Flow</span>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 }
--- src/pages/Login/LoginForm.tsx
+++ src/pages/Login/LoginForm.tsx
@@ -1,3 +1,14 @@
+import { useState } from "react";
+
 export default function LoginForm() {
-  return <h1>LoginForm</h1>;
+  const [email, setEmail] = useState('');
+  const [passwond, setPassword] = useState('');
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
+      <div className="max-w-md w-full bg-white rounded-xl shadow-lg p-8">
+        <h2 className="text-2xl font-bold text-gray-800">Acesse a sua conta</h2>
+      </div>
+    </div>
+  );
 }
